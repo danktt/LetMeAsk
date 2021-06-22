ac1e0da0a8e1653622f9722ed5966cc8d3df2ba0
danktt <danilomirandaa.15@outlook.com>
2021-06-22 16:22:37 -0400

    create-home

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,39 +1,20 @@
-// import { useState } from "react";
-
-
-// export function Button(){
-  
-  
-  //   const [counter, setCounter] = useState(0);
-  
-  
-  //   function increment(){
-    //     setCounter( counter + 1 )
-    //     // console.log(counter);
-    //   }
-    
-    
-    //   return (
-      //     <button onClick={increment}>
-      //       {counter}
-      //     </button>
-      //   )
-      // }
-      
-import { useState } from "react";
-
+import { useState } from "react"
 export function Button(){
-  const [counter, setCounter ] = useState(0)
-  
-  function addCounter(){
-    setCounter(counter + 5)
 
-    // counter++;
-    // console.log(counter);
-  }
+
+
+const [ counter, setCounter ] = useState(0);
+
+function increment(){
+  setCounter(counter +2);
+  console.log(counter)
+}
+
+
+
 
   return (
-    <button onClick={addCounter}>
+    <button onClick={increment}>
       {counter}
     </button>
   )
