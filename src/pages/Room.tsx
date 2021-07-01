9af23047d6510bb046bf1d5fd4a876c9d67d0a57
danktt <danilomirandaa.15@outlook.com>
2021-06-30 22:40:14 -0400

    bug-fix

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,39 +1,35 @@
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
-
-import '../styles/room.scss';
+import '../styles/room.scss'
 
 export function Room(){
-  return(
+  return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <div>Codigo</div>
         </div>
       </header>
 
       <main>
         <div className="room-title">
           <h1>Sala React</h1>
-          <span>4 pergustas</span>
+          <span>4 perguntas</span>
         </div>
 
-        <form >
+        <form>
           <textarea 
-            placeholder="O que você quer pertuntar?"         
+          placeholder="O que você quer perguntar"
           />
 
-          <div className="from-footer">
-            <span>Para enviar um pergunta,
-              <button>faça seu login</button>.
-            </span>
+          <div className="form-footer">
+            <span>Para enviar uma pergunta,<button>faça seu login</button>.</span>
             <Button type="submit">Enviar pergunta</Button>
           </div>
         </form>
       </main>
     </div>
-  )
+  );
 }
-
 
