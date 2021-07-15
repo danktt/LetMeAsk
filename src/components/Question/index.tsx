916cd4dfc8b47730b51dd73181e80fc9384ac73b
danktt <danilomirandaa.15@outlook.com>
2021-07-14 20:22:16 -0400

    question-icon

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,5 +1,7 @@
 
 import { ReactNode } from 'react';
+import cx from 'classnames'
+
 import './styles.scss';
 
 
@@ -10,16 +12,27 @@ type QuestionProps ={
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered: boolean;
+  isHighlighted: boolean;
 }
 
 
 export function Question({
   content,
   author,
+  isAnswered = false,
+  isHighlighted = false,
   children,
 }: QuestionProps) {
   return(
-    <div className="question">
+    <div 
+    className={cx(
+      'question',
+      {answered: isAnswered},
+      {highlighted: isHighlighted && !isAnswered},
+
+    )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
